fda428b493f9c0d368bd01756c91b2d428319911
Boris Abramov <borisab93@gmail.com>
2024-05-15 15:41:39 +0300

    Home, About, SocialLinks components added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import SocialLinks from "./components/SocialLinks";
+import About from "./components/About";
 
 function App() {
   return (
     <div>
-      <NavBar></NavBar>
+      <NavBar />
+      <Home />
+      <About />
+      <SocialLinks />
     </div>
   );
 }
