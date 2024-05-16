613c08b59b1870f3d36b1845e099192de5b261b6
Boris Abramov <borisab93@gmail.com>
2024-05-16 11:55:05 +0300

    added Portfolio, Skill and Contact Components

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,11 +2,38 @@ import React from "react";
 import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg";
 import installNode from "../assets/portfolio/installNode.jpg";
 import reactParallax from "../assets/portfolio/reactParallax.jpg";
-import navbar from "../assets/portfolio/navbar.jpg";
+import navBar from "../assets/portfolio/navbar.jpg";
 import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
 import reactWeather from "../assets/portfolio/reactWeather.jpg";
 
 const Portfolio = () => {
+  const portfolios = [
+    {
+      id: 1,
+      src: arrayDestruct,
+    },
+    {
+      id: 2,
+      src: installNode,
+    },
+    {
+      id: 3,
+      src: reactParallax,
+    },
+    {
+      id: 4,
+      src: navBar,
+    },
+    {
+      id: 5,
+      src: reactSmooth,
+    },
+    {
+      id: 6,
+      src: reactWeather,
+    },
+  ];
+
   return (
     <div
       name="portfolio"
@@ -21,21 +48,23 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          <div className="shadow-md shadow-gray-600 rounded-lg">
-            <img
-              src={reactWeather}
-              alt=""
-              className="rounded-md duration-200 hover:scale-105"
-            />
-            <div className="flex items-center justify-center">
-              <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                Demo
-              </button>
-              <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                Code
-              </button>
+          {portfolios.map(({ id, src }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <img
+                src={src}
+                alt=""
+                className="rounded-md duration-200 hover:scale-105"
+              />
+              <div className="flex items-center justify-center">
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Demo
+                </button>
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Code
+                </button>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
